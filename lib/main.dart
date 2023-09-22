d1db29bf3e4ddbb6ed65fbf5fed56ec43ae0a44f
Fernando Paz <ferpaz64@gmail.com>
2023-09-22 16:39:48 -0600

    Se agrego manejo de archivo .ENV con parametros globales

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:teslo_shop/config/config.dart';
-import 'package:teslo_shop/config/router/app_router.dart';
 
-void main() {
+Future<void> main() async {
+  await Environment.initEnvironment();
+
   runApp(
     const ProviderScope(
       child: MainApp(),
