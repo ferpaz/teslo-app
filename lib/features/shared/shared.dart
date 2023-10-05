f891fb0cd28c728d5c872ec768e08c69353811db
Fernando Paz <ferpaz64@gmail.com>
2023-10-05 14:54:25 -0600

    Se agregaron inputs para asociarlos en el formulario

--- lib/features/shared/shared.dart
+++ lib/features/shared/shared.dart
@@ -1,2 +1,5 @@
 export 'infrastructure/inputs/inputs.dart';
+export 'infrastructure/inputs/price.dart';
+export 'infrastructure/inputs/stock.dart';
+export 'infrastructure/inputs/title.dart';
 export 'widgets/widgets.dart';
